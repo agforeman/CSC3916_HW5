f2435bc07328698e75ead4d03ed81fb290f48ed9
tyhhytfirebird9 <alexander.foreman@ucdenver.edu>
2019-04-29 23:09:40 -0600

    Updates

---
+++ precache-manifest.b64e99598c553e7a1d2286e835300779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ccb622baa94ca52716c",
+    "url": "/CSC3916_HW5/static/js/runtime~main.0721b4a9.js"
+  },
+  {
+    "revision": "90d09305f87271019bc0",
+    "url": "/CSC3916_HW5/static/js/main.acfefb5b.chunk.js"
+  },
+  {
+    "revision": "a7a2c77b201da2e19ed1",
+    "url": "/CSC3916_HW5/static/js/2.a102bd25.chunk.js"
+  },
+  {
+    "revision": "90d09305f87271019bc0",
+    "url": "/CSC3916_HW5/static/css/main.76a30137.chunk.css"
+  },
+  {
+    "revision": "4c473a0d7aa4c4fd065cd286088088a5",
+    "url": "/CSC3916_HW5/index.html"
+  }
+];
